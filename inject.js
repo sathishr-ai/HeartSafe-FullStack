4d10453367747aa09ff2eef7d5af355e8928b1ea
Sathish R <sathxsh57@gmail.com>
2026-08-21 17:43:43 +0530

    Revert premium UI enhancement

--- inject.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let text = fs.readFileSync('CChd.prediction.html', 'utf8');
-if (!text.includes('premium-ui.css')) {
-    text = text.replace('</head>', '    <link rel="stylesheet" href="premium-ui.css">\n</head>');
-    fs.writeFileSync('CChd.prediction.html', text);
-    console.log('Injected premium-ui.css');
-} else {
-    console.log('Already injected');
-}
